b8a637d8b02c4ef839e39aebd4252ef3cd02c94f
jakartasipirok <kevinroni2002@gmail.com>
2022-03-25 20:50:46 +0700

    repair gui

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -54,6 +54,8 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(197, 26);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Input Filename";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button2
